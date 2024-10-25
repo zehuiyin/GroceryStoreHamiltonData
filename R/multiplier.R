8c4cdb803b610ffabbdfe6b16a6f7060b486e34d
zehuiyin <zehuiyin@gmail.com>
2024-10-25 13:02:58 -0400

    Initial commit

---
+++ R/multiplier.R
@@ -0,0 +1,14 @@
+#' Multiplying two numerical values or vectors
+#'
+#' @param x a numerical value or vector
+#' @param y a numerical value or vector
+#'
+#' @return a numerical value or vector
+#' @export
+#'
+#' @examples
+#' multiplier(2, 5)
+#' multiplier(c(2, 5), c(3, 8))
+multiplier <- function(x, y){
+  x * y
+}
---
+++ _dependencies.R
@@ -0,0 +1,4 @@
+# Adding missing packages to the renv lock file
+# https://rstudio.github.io/renv/reference/dependencies.html
+library(usethis)
+library(devtools)
