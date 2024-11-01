900a1398e794ed1e9193041ee0b190997cfb0e0e
Zehui Yin <zehuiyin@gmail.com>
2024-11-01 11:21:57 -0400

    update data_source preparation

--- R/data.R
+++ R/data.R
@@ -2,10 +2,18 @@
 #'
 #' This is a dataframe containing the information about all the raw data in this package
 #'
+#' @format A dataframe with 3 columns:
+#' \describe{
+#'   \item{Name}{Data Name}
+#'   \item{URL}{Source (URL)}
+#'   \item{Date}{Accessed Date}
+#' }
+#'
 #' @name data_source
 #' @docType data
 #' @keywords datasets
 #' @examples
 #' data(data_source)
 #' summary(data_source)
+#' head(data_source)
 NULL
