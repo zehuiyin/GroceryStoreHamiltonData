b715168f457787f6c1e811a5d8ae58e1203bb79a
Zehui Yin <zehuiyin@gmail.com>
2024-11-01 11:59:21 -0400

    add grocery_CT data

--- R/data.R
+++ R/data.R
@@ -1,12 +1,13 @@
 #' Data Source Information
 #'
-#' This is a dataframe containing the information about all the raw data in this package
+#' This is a dataframe containing the information about all the raw data in
+#' this package
 #'
 #' @format A dataframe with 3 columns:
 #' \describe{
-#'   \item{Name}{Data Name}
-#'   \item{URL}{Source (URL)}
-#'   \item{Date}{Accessed Date}
+#'   \item{Name}{Data Name: the name of the dataset.}
+#'   \item{URL}{Source (URL): the link where the data is downloaded.}
+#'   \item{Date}{Accessed Date: the date when the data is downloaded.}
 #' }
 #'
 #' @name data_source
@@ -16,4 +17,26 @@
 #' data(data_source)
 #' summary(data_source)
 #' head(data_source)
+NULL
+
+#' Grocery Stores in Census Tracts in Hamilton
+#'
+#' This is a sf dataframe containing the information about the number of grocery
+#' stores in each Census Tract in Hamilton
+#'
+#' @format A dataframe with 4 columns:
+#' \describe{
+#'   \item{GEOUID}{The unique ID for the geographical unit}
+#'   \item{POP21}{The population count in the census tract from 2021 Canadian Census}
+#'   \item{Grocery}{The number of grocery stores in that census tract}
+#'   \item{geometry}{Geometry column for sf dataframe}
+#' }
+#'
+#' @name grocery_CT
+#' @docType data
+#' @keywords datasets
+#' @examples
+#' data(grocery_CT)
+#' summary(grocery_CT[,c("GEOUID", "POP21", "Grocery")])
+#' head(grocery_CT[,c("GEOUID", "POP21", "Grocery")])
 NULL
