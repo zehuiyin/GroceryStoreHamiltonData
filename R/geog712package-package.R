858836aa1e2225ef423e42ec0fc3a50a2fefe632
zehuiyin <zehuiyin@gmail.com>
2024-11-08 12:59:05 -0500

    add test that and package name

---
+++ R/geog712package-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' geog712package: A package for GEOG 712 course activity
+#'
+#' This package is an exercise in package creation using
+#' R studio. The package includes a sample function and
+#' a sample dataset with their respective documentation.
+#'
+#' @docType package
+#' @name geog712package-package
+#' @author Zehui Yin, School of Earth, Environment and Society, McMaster University \email{yinz39@@mcmaster.ca}
+#' @references \url{https://github.com/zehuiyin/geog712package}
+NULL
---
+++ tests/testthat/test-multiplier.R
@@ -0,0 +1,3 @@
+test_that("multiplier works", {
+  expect_equal(multiplier(3, 2), 6)
+})
